9fca1fab4a1ae9a1d23ada014be1f85835b5b37c
howdyhi <money@221-17.wlan.yzu.edu.tw>
2016-11-03 14:01:26 +0800

    public class ViewPagerAdapter extends PagerAdapter

--- app/src/main/java/yzu/money/slideimage/MainActivity.java
+++ app/src/main/java/yzu/money/slideimage/MainActivity.java
@@ -1,12 +1,16 @@
 package yzu.money.slideimage;
 
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
 
 public class MainActivity extends AppCompatActivity {
     private LinearLayout ll;
@@ -23,6 +27,22 @@ public class MainActivity extends AppCompatActivity {
         tv.setTextSize(40);
         ll.addView(tv);
         vp=new ViewPager(this);
+        vp.setAdapter(new ViewPagerAdapter());
         ll.addView(vp);
     }
+    //-----------------------
+    public class ViewPagerAdapter extends PagerAdapter{
+
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return false;
+        }
+    }
 }
+
