f3098d631d9df9346d93de176afd5033d18aca87
howdyhi <money@221-17.wlan.yzu.edu.tw>
2016-11-03 13:51:35 +0800

    linear layout, declare vars

--- app/src/main/java/yzu/money/slideimage/MainActivity.java
+++ app/src/main/java/yzu/money/slideimage/MainActivity.java
@@ -1,13 +1,28 @@
 package yzu.money.slideimage;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    private LinearLayout ll;
+    private TextView tv;
+    private ViewPager vp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ll= (LinearLayout) findViewById(R.id.activity_main);
+        tv=new TextView(this);
+        tv.setTextSize(40);
+        ll.addView(tv);
+        vp=new ViewPager(this);
+        ll.addView(vp);
     }
 }
